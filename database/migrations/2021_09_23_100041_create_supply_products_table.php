fd50d5ffb4f0f91f8dedd9157d3c85525147c94f
Asshen <Asshen4522@yandex.ru>
2021-09-23 13:07:49 +0300

    migrations+vs

--- database/migrations/2021_09_23_100041_create_supply_products_table.php
+++ database/migrations/2021_09_23_100041_create_supply_products_table.php
@@ -1,10 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-class CreatePostsTable extends Migration
+class CreateSupplyProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,9 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
-            $table->bigIncrements('id');
+        Schema::create('supply_products', function (Blueprint $table) {
+            $table->id();
             $table->timestamps();
-            $table->string('email')->unique();
         });
     }
 
@@ -27,6 +26,6 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('supply_products');
     }
 }
